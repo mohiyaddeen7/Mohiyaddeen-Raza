9194132f72a4d0e9a6a05a0a8d1ee98c92aba37d
mohiyaddeen7 <mohiyaddeenraza7@gmail.com>
2023-10-09 19:16:53 +0530

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <div id="home">
-      Home
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mt-6 lg:mt-0">
+              <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                BRAND NAME
+              </h2>
+              <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                The Catcher in the Rye
+              </h1>
+              <div className="flex mb-4">
+                <span className="text-gray-600">4 Reviews</span>
+              </div>
+              <p className="leading-relaxed">
+                Fam locavore kickstarter distillery. Mixtape chillwave tumeric
+                sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
+                juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
+                seitan poutine tumeric. Gastropub blue bottle austin listicle
+                pour-over, neutra jean shorts keytar banjo tattooed umami
+                cardigan.
+              </p>
+            </div>
+            <img
+              alt="ecommerce"
+              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+              src="https://dummyimage.com/400x400"
+            />
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
