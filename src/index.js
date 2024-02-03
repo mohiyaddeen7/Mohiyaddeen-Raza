c46d9f436f3aff4e71024aa8350b1b6493de0730
mohiyaddeen7 <mohiyaddeenraza7@gmail.com>
2024-02-03 10:55:58 +0530

    vercel analytics attempt 1

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { sendToVercelAnalytics } from "./vitals";
+import { inject } from "@vercel/analytics";
 
+inject();
 import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App/>);
+root.render(<App />);
 reportWebVitals(sendToVercelAnalytics);
